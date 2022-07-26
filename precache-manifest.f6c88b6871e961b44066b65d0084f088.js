5760bb81e63c259f387736fd649fb31b165b0d56
Suryakaran Tiwari <suryakaran.tiwari@blazeclan.com>
2022-07-26 11:17:06 +0530

    first commit

---
+++ precache-manifest.f6c88b6871e961b44066b65d0084f088.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b688d3af62ac74117485",
+    "url": "/static/css/main.1aa3a553.chunk.css"
+  },
+  {
+    "revision": "b688d3af62ac74117485",
+    "url": "/static/js/main.75c0cb51.chunk.js"
+  },
+  {
+    "revision": "2f252785ab0106c33ae9",
+    "url": "/static/js/runtime~main.0da4c095.js"
+  },
+  {
+    "revision": "ccd98f8a7eb1f5b107fa",
+    "url": "/static/css/2.e1e34f45.chunk.css"
+  },
+  {
+    "revision": "ccd98f8a7eb1f5b107fa",
+    "url": "/static/js/2.e2c9e47f.chunk.js"
+  },
+  {
+    "revision": "a18b610acfe131f2d474",
+    "url": "/static/js/3.49b3ba72.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b4bcd27a7aacc60de171e9158b213450",
+    "url": "/index.html"
+  }
+];
